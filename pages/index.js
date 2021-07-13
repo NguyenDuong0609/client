a24887885b8b15922fb81f5092083687578f83af
NguyenDuong0609 <nguyenduong2824.com>
2021-07-13 15:46:03 +0700

    config socket io client connect

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import io from 'socket.io-client'
+
+let socket;
 
 export default function Home() {
+
+  const ENDPOINT = '103.81.86.16:5000';
+
+  useEffect(() => {
+    
+    socket = io(ENDPOINT);
+
+    socket.emit('join');
+    return () => {
+      cleanup
+    };
+  }, [ENDPOINT]);
   return (
     <>
       page of layout custom
